35356b40655e078152495c0d1e447fbc8ea345d9
Your Name <you@example.com>
2022-10-04 14:55:28 +0700

    Bài 4: Layout, Router (P2)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import DefaultLayout from '~/components/Layout/DefaultLayout';
@@ -8,7 +9,13 @@ function App() {
             <div className="App">
                 <Routes>
                     {publicRoutes.map((route, index) => {
-                        const Layout = route.Layout || DefaultLayout;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
                         const Page = route.component;
                         return (
                             <Route
